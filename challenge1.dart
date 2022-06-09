452a1b4459d65dbc459be711b22f40bab2cee780
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-06-09 14:18:14 +0300

    Dart functions Task

--- challenge1.dart
+++ challenge1.dart
@@ -29,3 +29,43 @@
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+
+void main() {
+  String name = "fahad";
+  int birthYear = 1997;
+
+  printName(name);
+  printAge(birthYear);
+  printHello("Fahad", "en");
+  printMax(7, 10);
+}
+
+void printName(String name) {
+  print("my name is $name "); //Task 1:
+}
+
+void printAge(int age) {
+  print("my age ${2022 - age}"); //Task 2:
+}
+
+void printHello(String name, String Langauge) {
+  //task 3:
+  if (Langauge == "en") {
+    print("Hello $name");
+  } else if (Langauge == "es") {
+    print("Hello $name");
+  } else if (Langauge == "fr") {
+    print("Hello $name");
+  } else if (Langauge == "tr") {
+    print("Hello $name");
+  }
+}
+
+void printMax(int a, int b) {
+  // task 4:
+  if (a > b) {
+    print(a);
+  } else {
+    print(b);
+  }
+}
--- challenge2.dart
+++ challenge2.dart
@@ -1,3 +1,10 @@
+void main() {
+  greet("Fahad");
+  isOdd(9);
+  squareOrDouble(9);
+  oddsSmallerThan(8);
+}
+
 /**
  * greet(name):
  * - receives a name
@@ -7,7 +14,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 void greet(String name) {
-  // Your code here
+  print("Hello $name");
 }
 
 /**
@@ -20,7 +27,11 @@ void greet(String name) {
  * isOdd(10) -> false
  */
 bool isOdd(int n) {
-  // Your code here
+  if (n % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -33,7 +44,11 @@ bool isOdd(int n) {
  * oddsSmallerThan(15) -> 7
  */
 int oddsSmallerThan(int n) {
-  // Your code here
+  if (isOdd(n)) {
+    return (n - 1) ~/ 2;
+  } else {
+    return n ~/ 2;
+  }
 }
 
 /**
@@ -47,5 +62,9 @@ int oddsSmallerThan(int n) {
  * squareOrDouble(9) -> 81
  */
 int squareOrDouble(int n) {
-  // Your code here
+  if (isOdd(n)) {
+    return n * n;
+  } else {
+    return n * 2;
+  }
 }
